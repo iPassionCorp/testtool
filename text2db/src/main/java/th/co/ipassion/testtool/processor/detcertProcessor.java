e6e21c90e7a6b3a9a003cf9abc66b4f42da3d513
Kunchai <kunchai.sdhm@gmail.com>
2018-03-13 15:09:56 +0700

    Add processing detcert/mgreceiver and fix bug /u0000 and string null

---
+++ text2db/src/main/java/th/co/ipassion/testtool/processor/detcertProcessor.java
@@ -0,0 +1,38 @@
+package th.co.ipassion.testtool.processor;
+
+import org.springframework.batch.item.ItemProcessor;
+import th.co.ipassion.testtool.dto.tlitext.DetCert;
+
+
+public class detcertProcessor implements ItemProcessor<DetCert, DetCert> {
+
+    public DetCert process(DetCert item) throws Exception {
+        if(item.getPolicyno().matches("(.*)\\u0000(.*)") ||
+                item.getAddress1().matches("(.*)\\u0000(.*)")||
+                item.getAddress2().matches("(.*)\\u0000(.*)")||
+                item.getTelephoneno().matches("(.*)\\u0000(.*)")||
+                item.getMariagestatus().matches("(.*)\\u0000(.*)")||
+                item.getRecname1().matches("(.*)\\u0000(.*)")||
+                item.getPercent1().matches("(.*)\\u0000(.*)")||
+                item.getRelationshipcode1().matches("(.*)\\u0000(.*)")||
+                item.getRecname2().matches("(.*)\\u0000(.*)")||
+                item.getPercent2().matches("(.*)\\u0000(.*)")||
+                item.getRelationshipcode2().matches("(.*)\\u0000(.*)")||
+                item.getRecname3().matches("(.*)\\u0000(.*)")||
+                item.getPercent3().matches("(.*)\\u0000(.*)")||
+                item.getRelationshipcode3().matches("(.*)\\u0000(.*)")||
+                item.getAnalist().matches("(.*)\\u0000(.*)")||
+                item.getPrmiseno().matches("(.*)\\u0000(.*)")||
+                item.getPrmiseno2().matches("(.*)\\u0000(.*)")||
+                item.getLoantype().matches("(.*)\\u0000(.*)")||
+                item.getMed().matches("(.*)\\u0000(.*)")||
+                item.getReserve().matches("(.*)\\u0000(.*)")
+                ){
+            return null;
+        }
+        System.out.println("Done!!!");
+        return item;
+    }
+}
+
+
---
+++ text2db/src/main/java/th/co/ipassion/testtool/processor/mgReceiverProcessor.java
@@ -0,0 +1,45 @@
+package th.co.ipassion.testtool.processor;
+
+import org.springframework.batch.item.ItemProcessor;
+import th.co.ipassion.testtool.dto.tlitext.MgReceiver;
+
+public class mgReceiverProcessor implements ItemProcessor<MgReceiver, MgReceiver>{
+
+    public MgReceiver process(MgReceiver item) throws Exception {
+        if (
+        item.getCertno().matches("(.*)\\u0000(.*)") ||
+        item.getAccidentdate().matches("(.*)\\u0000(.*)") ||
+        item.getReceivedate().matches("(.*)\\u0000(.*)") ||
+        item.getSeqno().matches("(.*)\\u0000(.*)") ||
+        item.getPrename().matches("(.*)\\u0000(.*)") ||
+        item.getFirstname().matches("(.*)\\u0000(.*)") ||
+        item.getLastname().matches("(.*)\\u0000(.*)") ||
+        item.getType().matches("(.*)\\u0000(.*)") ||
+        item.getPayamount().matches("(.*)\\u0000(.*)") ||
+        item.getAddress().matches("(.*)\\u0000(.*)") ||
+        item.getTumbon().matches("(.*)\\u0000(.*)") ||
+        item.getZipcode().matches("(.*)\\u0000(.*)") ||
+        item.getMobilephone().matches("(.*)\\u0000(.*)") ||
+        item.getCitizenid().matches("(.*)\\u0000(.*)") ||
+        item.getPaytype().matches("(.*)\\u0000(.*)") ||
+        item.getBanktype().matches("(.*)\\u0000(.*)") ||
+        item.getBankcode().matches("(.*)\\u0000(.*)") ||
+        item.getBankbranch().matches("(.*)\\u0000(.*)") ||
+        item.getBankaccount().matches("(.*)\\u0000(.*)") ||
+        item.getGuardianbeneficiary().matches("(.*)\\u0000(.*)") ||
+        item.getIspay().matches("(.*)\\u0000(.*)") ||
+        item.getHaveindicia().matches("(.*)\\u0000(.*)") ||
+        item.getCountrycode().matches("(.*)\\u0000(.*)") ||
+        item.getClaimpaydate().matches("(.*)\\u0000(.*)") ||
+        item.getBookstatus().matches("(.*)\\u0000(.*)") ||
+        item.getChqdate().matches("(.*)\\u0000(.*)") ||
+        item.getChqno().matches("(.*)\\u0000(.*)") ||
+        item.getDocno().matches("(.*)\\u0000(.*)") ||
+        item.getIsbeneficiarykilled().matches("(.*)\\u0000(.*)") ||
+        item.getBirthdate().matches("(.*)\\u0000(.*)")
+        ){
+            return null;
+        }
+        return item;
+    }
+}
