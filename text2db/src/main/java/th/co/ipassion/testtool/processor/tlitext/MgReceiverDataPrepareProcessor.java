13db127d3ae5758ffd9696656f4a5fe2ce7d085b
Kunchai Sodhom <kunchai.sdhm@gmail.com>
2018-03-20 13:55:59 +0700

    Delete some code for MgReciver becasuse no idea to resolve it with spring batch

--- text2db/src/main/java/th/co/ipassion/testtool/processor/tlitext/MgReceiverDataPrepareProcessor.java
+++ text2db/src/main/java/th/co/ipassion/testtool/processor/tlitext/MgReceiverDataPrepareProcessor.java
@@ -1,12 +1,6 @@
 package th.co.ipassion.testtool.processor.tlitext;
 
-import org.springframework.batch.item.ItemProcessor;
-import th.co.ipassion.testtool.dto.tlitext.MgReceiver;
 
+public class MgReceiverDataPrepareProcessor{
 
-public class MgReceiverDataPrepareProcessor implements ItemProcessor<MgReceiver, MgReceiver> {
-    public MgReceiver process(MgReceiver item) {
-        System.out.println(item.getCertno());
-        return item;
-    }
 }
