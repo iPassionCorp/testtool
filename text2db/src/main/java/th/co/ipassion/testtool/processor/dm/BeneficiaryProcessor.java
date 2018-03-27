815c9a03714d3f34f75985d4039a03786eb47cda
Kunchai Sodhom <kunchai.sdhm@gmail.com>
2018-03-27 14:08:41 +0700

    Add Processor on dm and remove policy on DetCert

---
+++ text2db/src/main/java/th/co/ipassion/testtool/processor/dm/BeneficiaryProcessor.java
@@ -0,0 +1,28 @@
+package th.co.ipassion.testtool.processor.dm;
+
+import org.springframework.batch.item.ItemProcessor;
+import th.co.ipassion.testtool.dto.tlitext.Beneficiary;
+
+public class BeneficiaryProcessor  implements ItemProcessor<Beneficiary, Beneficiary> {
+    public Beneficiary process(Beneficiary item) {
+        String text = item.getPolicytype()+" "+item.getPolicyno()+" "+item.getSequence()+" "+item.getPrename()
+                +" "+item.getFirstname()+" "+item.getLastname()+" "+item.getRelationshipcode()+" "+item.getPercentshare()
+                +" "+item.getPersonflag()+" "+item.getIdno()+" "+item.getBirthdate();
+        if (text.matches("(.*)\\u0000(.*)")) {
+            item.setPolicytype(item.getPolicytype().trim().replaceAll("\\u0000", ""));
+            item.setPolicyno(item.getPolicyno().trim().replaceAll("\\u0000", ""));
+            item.setSequence(item.getSequence().trim().replaceAll("\\u0000", ""));
+            item.setPrename(item.getPrename().trim().replaceAll("\\u0000", ""));
+            item.setFirstname(item.getFirstname().trim().replaceAll("\\u0000", ""));
+            item.setLastname(item.getLastname().trim().replaceAll("\\u0000", ""));
+            item.setRelationshipcode(item.getRelationshipcode().trim().replaceAll("\\u0000", ""));
+            item.setPercentshare(item.getPercentshare().trim().replaceAll("\\u0000", ""));
+            item.setPersonflag(item.getPersonflag().trim().replaceAll("\\u0000", ""));
+            item.setIdno(item.getIdno().trim().replaceAll("\\u0000", ""));
+            item.setBirthdate(item.getBirthdate().trim().replaceAll("\\u0000", ""));
+            return item;
+        }else{
+            return item;
+        }
+    }
+}
