f540ac951e1e075981b7281ee5fe751d98f2cd47
Kunchai Sodhom <kunchai.sdhm@gmail.com>
2018-04-23 16:13:06 +0700

    Add enviroment for data endpoint and create entity mapper

--- text2db/src/main/java/th/co/ipassion/testtool/mapper/dmDetCert.java
+++ text2db/src/main/java/th/co/ipassion/testtool/mapper/dmDetCert.java
@@ -15,7 +15,23 @@ public class dmDetCert implements RowMapper<DetCert>{
         dc.setCode(rs.getString("code"));
         dc.setAddress1(rs.getString("address1"));
         dc.setAddress2(rs.getString("address2"));
-        System.out.println(dc);
+        dc.setTelephoneno(rs.getString("telephoneno"));
+        dc.setMariagestatus(rs.getString("mariagestatus"));
+        dc.setRecname1(rs.getString("recname1"));
+        dc.setPercent1(rs.getString("percent1"));
+        dc.setRelationshipcode1(rs.getString("relationshipcode1"));
+        dc.setRecname1(rs.getString("recname2"));
+        dc.setPercent1(rs.getString("percent2"));
+        dc.setRelationshipcode1(rs.getString("relationshipcode2"));
+        dc.setRecname1(rs.getString("recname3"));
+        dc.setPercent1(rs.getString("percent3"));
+        dc.setRelationshipcode1(rs.getString("relationshipcode3"));
+        dc.setAnalist(rs.getString("analist"));
+        dc.setPrmiseno(rs.getString("prmiseno"));
+        dc.setPrmiseno2(rs.getString("prmiseno2"));
+        dc.setLoantype(rs.getString("loantype"));
+        dc.setMed(rs.getString("med"));
+        dc.setReserve(rs.getString("reserve"));
         return dc;
 
     }
