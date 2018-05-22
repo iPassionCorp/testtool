008755672c417a1947dd3bdba8ca6c60fdf9345b
Kunchai <kunchai.sdhm@gmail.com>
2018-05-22 14:45:09 +0700

    Add job dmTlpMigration and repDmTlpMigration

---
+++ text2db/src/main/java/th/co/ipassion/testtool/dto/dm/TlpMigration.java
@@ -0,0 +1,85 @@
+package th.co.ipassion.testtool.dto.dm;
+
+public class TlpMigration {
+    private String customerCode;
+    private String tlpPolicyNo;
+    private String tlpCertNo;
+    private String tlpPlanNo;
+    private String tlpProductName;
+    private String migratePlanCode;
+    private String migratePlanNo;
+    private String tmoPlanCode;
+    private String tmoPlanNo;
+
+    public String getCustomerCode() {
+        return customerCode;
+    }
+
+    public void setCustomerCode(String customerCode) {
+        this.customerCode = customerCode;
+    }
+
+    public String getTlpPolicyNo() {
+        return tlpPolicyNo;
+    }
+
+    public void setTlpPolicyNo(String tlpPolicyNo) {
+        this.tlpPolicyNo = tlpPolicyNo;
+    }
+
+    public String getTlpCertNo() {
+        return tlpCertNo;
+    }
+
+    public void setTlpCertNo(String tlpCertNo) {
+        this.tlpCertNo = tlpCertNo;
+    }
+
+    public String getTlpPlanNo() {
+        return tlpPlanNo;
+    }
+
+    public void setTlpPlanNo(String tlpPlanNo) {
+        this.tlpPlanNo = tlpPlanNo;
+    }
+
+    public String getTlpProductName() {
+        return tlpProductName;
+    }
+
+    public void setTlpProductName(String tlpProductName) {
+        this.tlpProductName = tlpProductName;
+    }
+
+    public String getMigratePlanCode() {
+        return migratePlanCode;
+    }
+
+    public void setMigratePlanCode(String migratePlanCode) {
+        this.migratePlanCode = migratePlanCode;
+    }
+
+    public String getMigratePlanNo() {
+        return migratePlanNo;
+    }
+
+    public void setMigratePlanNo(String migratePlanNo) {
+        this.migratePlanNo = migratePlanNo;
+    }
+
+    public String getTmoPlanCode() {
+        return tmoPlanCode;
+    }
+
+    public void setTmoPlanCode(String tmoPlanCode) {
+        this.tmoPlanCode = tmoPlanCode;
+    }
+
+    public String getTmoPlanNo() {
+        return tmoPlanNo;
+    }
+
+    public void setTmoPlanNo(String tmoPlanNo) {
+        this.tmoPlanNo = tmoPlanNo;
+    }
+}
