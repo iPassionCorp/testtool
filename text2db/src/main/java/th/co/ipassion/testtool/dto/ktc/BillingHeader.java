2a21ed7cdad38cc8650f0c27b7f18b232588b741
Samart Nuntakhwang <samart@ipassion.co.th>
2018-05-23 15:23:36 +0700

    Add models for ktc job.

---
+++ text2db/src/main/java/th/co/ipassion/testtool/dto/ktc/BillingHeader.java
@@ -0,0 +1,176 @@
+package th.co.ipassion.testtool.dto.ktc;
+
+public class BillingHeader  implements java.io.Serializable {
+
+
+     private String uttxrectype;
+     private String uttxseq;
+     private String uttxeffectivedate;
+     private String uttxfiledate;
+     private String uttxmerid;
+     private String uttxmername;
+     private String uttxcompacno;
+     private String uttxservicetype;
+     private String uttxbankcode;
+     private String uttxrecsize;
+     private String uttxfiletype;
+     private String filler;
+
+    public BillingHeader() {
+    }
+
+    public BillingHeader(String uttxrectype, String uttxseq, String uttxeffectivedate, String uttxfiledate, String uttxmerid, String uttxmername, String uttxcompacno, String uttxservicetype, String uttxbankcode, String uttxrecsize, String uttxfiletype, String filler) {
+       this.uttxrectype = uttxrectype;
+       this.uttxseq = uttxseq;
+       this.uttxeffectivedate = uttxeffectivedate;
+       this.uttxfiledate = uttxfiledate;
+       this.uttxmerid = uttxmerid;
+       this.uttxmername = uttxmername;
+       this.uttxcompacno = uttxcompacno;
+       this.uttxservicetype = uttxservicetype;
+       this.uttxbankcode = uttxbankcode;
+       this.uttxrecsize = uttxrecsize;
+       this.uttxfiletype = uttxfiletype;
+       this.filler = filler;
+    }
+   
+
+    public String getUttxrectype() {
+        return this.uttxrectype;
+    }
+    
+    public void setUttxrectype(String uttxrectype) {
+        this.uttxrectype = uttxrectype;
+    }
+
+    public String getUttxseq() {
+        return this.uttxseq;
+    }
+    
+    public void setUttxseq(String uttxseq) {
+        this.uttxseq = uttxseq;
+    }
+
+    public String getUttxeffectivedate() {
+        return this.uttxeffectivedate;
+    }
+    
+    public void setUttxeffectivedate(String uttxeffectivedate) {
+        this.uttxeffectivedate = uttxeffectivedate;
+    }
+
+    public String getUttxfiledate() {
+        return this.uttxfiledate;
+    }
+    
+    public void setUttxfiledate(String uttxfiledate) {
+        this.uttxfiledate = uttxfiledate;
+    }
+
+    public String getUttxmerid() {
+        return this.uttxmerid;
+    }
+    
+    public void setUttxmerid(String uttxmerid) {
+        this.uttxmerid = uttxmerid;
+    }
+
+    public String getUttxmername() {
+        return this.uttxmername;
+    }
+    
+    public void setUttxmername(String uttxmername) {
+        this.uttxmername = uttxmername;
+    }
+
+    public String getUttxcompacno() {
+        return this.uttxcompacno;
+    }
+    
+    public void setUttxcompacno(String uttxcompacno) {
+        this.uttxcompacno = uttxcompacno;
+    }
+
+    public String getUttxservicetype() {
+        return this.uttxservicetype;
+    }
+    
+    public void setUttxservicetype(String uttxservicetype) {
+        this.uttxservicetype = uttxservicetype;
+    }
+
+    public String getUttxbankcode() {
+        return this.uttxbankcode;
+    }
+    
+    public void setUttxbankcode(String uttxbankcode) {
+        this.uttxbankcode = uttxbankcode;
+    }
+
+    public String getUttxrecsize() {
+        return this.uttxrecsize;
+    }
+    
+    public void setUttxrecsize(String uttxrecsize) {
+        this.uttxrecsize = uttxrecsize;
+    }
+
+    public String getUttxfiletype() {
+        return this.uttxfiletype;
+    }
+    
+    public void setUttxfiletype(String uttxfiletype) {
+        this.uttxfiletype = uttxfiletype;
+    }
+
+    public String getFiller() {
+        return this.filler;
+    }
+    
+    public void setFiller(String filler) {
+        this.filler = filler;
+    }
+
+
+   public boolean equals(Object other) {
+         if ( (this == other ) ) return true;
+		 if ( (other == null ) ) return false;
+		 if ( !(other instanceof BillingHeader) ) return false;
+		 BillingHeader castOther = ( BillingHeader ) other; 
+         
+		 return ( (this.getUttxrectype()==castOther.getUttxrectype()) || ( this.getUttxrectype()!=null && castOther.getUttxrectype()!=null && this.getUttxrectype().equals(castOther.getUttxrectype()) ) )
+ && ( (this.getUttxseq()==castOther.getUttxseq()) || ( this.getUttxseq()!=null && castOther.getUttxseq()!=null && this.getUttxseq().equals(castOther.getUttxseq()) ) )
+ && ( (this.getUttxeffectivedate()==castOther.getUttxeffectivedate()) || ( this.getUttxeffectivedate()!=null && castOther.getUttxeffectivedate()!=null && this.getUttxeffectivedate().equals(castOther.getUttxeffectivedate()) ) )
+ && ( (this.getUttxfiledate()==castOther.getUttxfiledate()) || ( this.getUttxfiledate()!=null && castOther.getUttxfiledate()!=null && this.getUttxfiledate().equals(castOther.getUttxfiledate()) ) )
+ && ( (this.getUttxmerid()==castOther.getUttxmerid()) || ( this.getUttxmerid()!=null && castOther.getUttxmerid()!=null && this.getUttxmerid().equals(castOther.getUttxmerid()) ) )
+ && ( (this.getUttxmername()==castOther.getUttxmername()) || ( this.getUttxmername()!=null && castOther.getUttxmername()!=null && this.getUttxmername().equals(castOther.getUttxmername()) ) )
+ && ( (this.getUttxcompacno()==castOther.getUttxcompacno()) || ( this.getUttxcompacno()!=null && castOther.getUttxcompacno()!=null && this.getUttxcompacno().equals(castOther.getUttxcompacno()) ) )
+ && ( (this.getUttxservicetype()==castOther.getUttxservicetype()) || ( this.getUttxservicetype()!=null && castOther.getUttxservicetype()!=null && this.getUttxservicetype().equals(castOther.getUttxservicetype()) ) )
+ && ( (this.getUttxbankcode()==castOther.getUttxbankcode()) || ( this.getUttxbankcode()!=null && castOther.getUttxbankcode()!=null && this.getUttxbankcode().equals(castOther.getUttxbankcode()) ) )
+ && ( (this.getUttxrecsize()==castOther.getUttxrecsize()) || ( this.getUttxrecsize()!=null && castOther.getUttxrecsize()!=null && this.getUttxrecsize().equals(castOther.getUttxrecsize()) ) )
+ && ( (this.getUttxfiletype()==castOther.getUttxfiletype()) || ( this.getUttxfiletype()!=null && castOther.getUttxfiletype()!=null && this.getUttxfiletype().equals(castOther.getUttxfiletype()) ) )
+ && ( (this.getFiller()==castOther.getFiller()) || ( this.getFiller()!=null && castOther.getFiller()!=null && this.getFiller().equals(castOther.getFiller()) ) );
+   }
+   
+   public int hashCode() {
+         int result = 17;
+         
+         result = 37 * result + ( getUttxrectype() == null ? 0 : this.getUttxrectype().hashCode() );
+         result = 37 * result + ( getUttxseq() == null ? 0 : this.getUttxseq().hashCode() );
+         result = 37 * result + ( getUttxeffectivedate() == null ? 0 : this.getUttxeffectivedate().hashCode() );
+         result = 37 * result + ( getUttxfiledate() == null ? 0 : this.getUttxfiledate().hashCode() );
+         result = 37 * result + ( getUttxmerid() == null ? 0 : this.getUttxmerid().hashCode() );
+         result = 37 * result + ( getUttxmername() == null ? 0 : this.getUttxmername().hashCode() );
+         result = 37 * result + ( getUttxcompacno() == null ? 0 : this.getUttxcompacno().hashCode() );
+         result = 37 * result + ( getUttxservicetype() == null ? 0 : this.getUttxservicetype().hashCode() );
+         result = 37 * result + ( getUttxbankcode() == null ? 0 : this.getUttxbankcode().hashCode() );
+         result = 37 * result + ( getUttxrecsize() == null ? 0 : this.getUttxrecsize().hashCode() );
+         result = 37 * result + ( getUttxfiletype() == null ? 0 : this.getUttxfiletype().hashCode() );
+         result = 37 * result + ( getFiller() == null ? 0 : this.getFiller().hashCode() );
+         return result;
+   }   
+
+
+}
+
+
---
+++ text2db/src/main/java/th/co/ipassion/testtool/dto/ktc/PaymentDetail.java
@@ -0,0 +1,211 @@
+package th.co.ipassion.testtool.dto.ktc;
+
+public class PaymentDetail  implements java.io.Serializable {
+
+
+     private String uttxrectype;
+     private String uttxseq;
+     private String uttxpan;
+     private String uttxcardholdername;
+     private String uttxtrxnamt;
+     private String uttxreferno1;
+     private String uttxreferno2;
+     private String uttxtrxntype;
+     private String uttxrecordstatus;
+     private String uttxoutputresult;
+     private String uttxpaymentdate;
+     private String uttxpaymenttime;
+     private String uttxresponsecode;
+     private String uttxauthcode;
+     private String expirydate;
+
+    public PaymentDetail() {
+    }
+
+    public PaymentDetail(String uttxrectype, String uttxseq, String uttxpan, String uttxcardholdername, String uttxtrxnamt, String uttxreferno1, String uttxreferno2, String uttxtrxntype, String uttxrecordstatus, String uttxoutputresult, String uttxpaymentdate, String uttxpaymenttime, String uttxresponsecode, String uttxauthcode, String expirydate) {
+       this.uttxrectype = uttxrectype;
+       this.uttxseq = uttxseq;
+       this.uttxpan = uttxpan;
+       this.uttxcardholdername = uttxcardholdername;
+       this.uttxtrxnamt = uttxtrxnamt;
+       this.uttxreferno1 = uttxreferno1;
+       this.uttxreferno2 = uttxreferno2;
+       this.uttxtrxntype = uttxtrxntype;
+       this.uttxrecordstatus = uttxrecordstatus;
+       this.uttxoutputresult = uttxoutputresult;
+       this.uttxpaymentdate = uttxpaymentdate;
+       this.uttxpaymenttime = uttxpaymenttime;
+       this.uttxresponsecode = uttxresponsecode;
+       this.uttxauthcode = uttxauthcode;
+       this.expirydate = expirydate;
+    }
+   
+    public String getUttxrectype() {
+        return this.uttxrectype;
+    }
+    
+    public void setUttxrectype(String uttxrectype) {
+        this.uttxrectype = uttxrectype;
+    }
+
+    public String getUttxseq() {
+        return this.uttxseq;
+    }
+    
+    public void setUttxseq(String uttxseq) {
+        this.uttxseq = uttxseq;
+    }
+
+    public String getUttxpan() {
+        return this.uttxpan;
+    }
+    
+    public void setUttxpan(String uttxpan) {
+        this.uttxpan = uttxpan;
+    }
+
+    public String getUttxcardholdername() {
+        return this.uttxcardholdername;
+    }
+    
+    public void setUttxcardholdername(String uttxcardholdername) {
+        this.uttxcardholdername = uttxcardholdername;
+    }
+
+    public String getUttxtrxnamt() {
+        return this.uttxtrxnamt;
+    }
+    
+    public void setUttxtrxnamt(String uttxtrxnamt) {
+        this.uttxtrxnamt = uttxtrxnamt;
+    }
+
+    public String getUttxreferno1() {
+        return this.uttxreferno1;
+    }
+    
+    public void setUttxreferno1(String uttxreferno1) {
+        this.uttxreferno1 = uttxreferno1;
+    }
+
+    public String getUttxreferno2() {
+        return this.uttxreferno2;
+    }
+    
+    public void setUttxreferno2(String uttxreferno2) {
+        this.uttxreferno2 = uttxreferno2;
+    }
+
+    public String getUttxtrxntype() {
+        return this.uttxtrxntype;
+    }
+    
+    public void setUttxtrxntype(String uttxtrxntype) {
+        this.uttxtrxntype = uttxtrxntype;
+    }
+
+    public String getUttxrecordstatus() {
+        return this.uttxrecordstatus;
+    }
+    
+    public void setUttxrecordstatus(String uttxrecordstatus) {
+        this.uttxrecordstatus = uttxrecordstatus;
+    }
+
+    public String getUttxoutputresult() {
+        return this.uttxoutputresult;
+    }
+    
+    public void setUttxoutputresult(String uttxoutputresult) {
+        this.uttxoutputresult = uttxoutputresult;
+    }
+
+    public String getUttxpaymentdate() {
+        return this.uttxpaymentdate;
+    }
+    
+    public void setUttxpaymentdate(String uttxpaymentdate) {
+        this.uttxpaymentdate = uttxpaymentdate;
+    }
+
+    public String getUttxpaymenttime() {
+        return this.uttxpaymenttime;
+    }
+    
+    public void setUttxpaymenttime(String uttxpaymenttime) {
+        this.uttxpaymenttime = uttxpaymenttime;
+    }
+
+    public String getUttxresponsecode() {
+        return this.uttxresponsecode;
+    }
+    
+    public void setUttxresponsecode(String uttxresponsecode) {
+        this.uttxresponsecode = uttxresponsecode;
+    }
+
+    public String getUttxauthcode() {
+        return this.uttxauthcode;
+    }
+    
+    public void setUttxauthcode(String uttxauthcode) {
+        this.uttxauthcode = uttxauthcode;
+    }
+
+    public String getExpirydate() {
+        return this.expirydate;
+    }
+    
+    public void setExpirydate(String expirydate) {
+        this.expirydate = expirydate;
+    }
+
+
+   public boolean equals(Object other) {
+         if ( (this == other ) ) return true;
+		 if ( (other == null ) ) return false;
+		 if ( !(other instanceof PaymentDetail) ) return false;
+		 PaymentDetail castOther = ( PaymentDetail ) other; 
+         
+		 return ( (this.getUttxrectype()==castOther.getUttxrectype()) || ( this.getUttxrectype()!=null && castOther.getUttxrectype()!=null && this.getUttxrectype().equals(castOther.getUttxrectype()) ) )
+ && ( (this.getUttxseq()==castOther.getUttxseq()) || ( this.getUttxseq()!=null && castOther.getUttxseq()!=null && this.getUttxseq().equals(castOther.getUttxseq()) ) )
+ && ( (this.getUttxpan()==castOther.getUttxpan()) || ( this.getUttxpan()!=null && castOther.getUttxpan()!=null && this.getUttxpan().equals(castOther.getUttxpan()) ) )
+ && ( (this.getUttxcardholdername()==castOther.getUttxcardholdername()) || ( this.getUttxcardholdername()!=null && castOther.getUttxcardholdername()!=null && this.getUttxcardholdername().equals(castOther.getUttxcardholdername()) ) )
+ && ( (this.getUttxtrxnamt()==castOther.getUttxtrxnamt()) || ( this.getUttxtrxnamt()!=null && castOther.getUttxtrxnamt()!=null && this.getUttxtrxnamt().equals(castOther.getUttxtrxnamt()) ) )
+ && ( (this.getUttxreferno1()==castOther.getUttxreferno1()) || ( this.getUttxreferno1()!=null && castOther.getUttxreferno1()!=null && this.getUttxreferno1().equals(castOther.getUttxreferno1()) ) )
+ && ( (this.getUttxreferno2()==castOther.getUttxreferno2()) || ( this.getUttxreferno2()!=null && castOther.getUttxreferno2()!=null && this.getUttxreferno2().equals(castOther.getUttxreferno2()) ) )
+ && ( (this.getUttxtrxntype()==castOther.getUttxtrxntype()) || ( this.getUttxtrxntype()!=null && castOther.getUttxtrxntype()!=null && this.getUttxtrxntype().equals(castOther.getUttxtrxntype()) ) )
+ && ( (this.getUttxrecordstatus()==castOther.getUttxrecordstatus()) || ( this.getUttxrecordstatus()!=null && castOther.getUttxrecordstatus()!=null && this.getUttxrecordstatus().equals(castOther.getUttxrecordstatus()) ) )
+ && ( (this.getUttxoutputresult()==castOther.getUttxoutputresult()) || ( this.getUttxoutputresult()!=null && castOther.getUttxoutputresult()!=null && this.getUttxoutputresult().equals(castOther.getUttxoutputresult()) ) )
+ && ( (this.getUttxpaymentdate()==castOther.getUttxpaymentdate()) || ( this.getUttxpaymentdate()!=null && castOther.getUttxpaymentdate()!=null && this.getUttxpaymentdate().equals(castOther.getUttxpaymentdate()) ) )
+ && ( (this.getUttxpaymenttime()==castOther.getUttxpaymenttime()) || ( this.getUttxpaymenttime()!=null && castOther.getUttxpaymenttime()!=null && this.getUttxpaymenttime().equals(castOther.getUttxpaymenttime()) ) )
+ && ( (this.getUttxresponsecode()==castOther.getUttxresponsecode()) || ( this.getUttxresponsecode()!=null && castOther.getUttxresponsecode()!=null && this.getUttxresponsecode().equals(castOther.getUttxresponsecode()) ) )
+ && ( (this.getUttxauthcode()==castOther.getUttxauthcode()) || ( this.getUttxauthcode()!=null && castOther.getUttxauthcode()!=null && this.getUttxauthcode().equals(castOther.getUttxauthcode()) ) )
+ && ( (this.getExpirydate()==castOther.getExpirydate()) || ( this.getExpirydate()!=null && castOther.getExpirydate()!=null && this.getExpirydate().equals(castOther.getExpirydate()) ) );
+   }
+   
+   public int hashCode() {
+         int result = 17;
+         
+         result = 37 * result + ( getUttxrectype() == null ? 0 : this.getUttxrectype().hashCode() );
+         result = 37 * result + ( getUttxseq() == null ? 0 : this.getUttxseq().hashCode() );
+         result = 37 * result + ( getUttxpan() == null ? 0 : this.getUttxpan().hashCode() );
+         result = 37 * result + ( getUttxcardholdername() == null ? 0 : this.getUttxcardholdername().hashCode() );
+         result = 37 * result + ( getUttxtrxnamt() == null ? 0 : this.getUttxtrxnamt().hashCode() );
+         result = 37 * result + ( getUttxreferno1() == null ? 0 : this.getUttxreferno1().hashCode() );
+         result = 37 * result + ( getUttxreferno2() == null ? 0 : this.getUttxreferno2().hashCode() );
+         result = 37 * result + ( getUttxtrxntype() == null ? 0 : this.getUttxtrxntype().hashCode() );
+         result = 37 * result + ( getUttxrecordstatus() == null ? 0 : this.getUttxrecordstatus().hashCode() );
+         result = 37 * result + ( getUttxoutputresult() == null ? 0 : this.getUttxoutputresult().hashCode() );
+         result = 37 * result + ( getUttxpaymentdate() == null ? 0 : this.getUttxpaymentdate().hashCode() );
+         result = 37 * result + ( getUttxpaymenttime() == null ? 0 : this.getUttxpaymenttime().hashCode() );
+         result = 37 * result + ( getUttxresponsecode() == null ? 0 : this.getUttxresponsecode().hashCode() );
+         result = 37 * result + ( getUttxauthcode() == null ? 0 : this.getUttxauthcode().hashCode() );
+         result = 37 * result + ( getExpirydate() == null ? 0 : this.getExpirydate().hashCode() );
+         return result;
+   }   
+
+
+}
+
+
