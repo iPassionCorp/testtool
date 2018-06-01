f6dc27ba719670726244d1618e0d527b561992f5
Samart Nuntakhwang <samart@ipassion.co.th>
2018-06-01 13:58:57 +0700

    Fix Bug

--- text2db/src/main/java/th/co/ipassion/testtool/dto/tlitext/Ords.java
+++ text2db/src/main/java/th/co/ipassion/testtool/dto/tlitext/Ords.java
@@ -9,4 +9,53 @@ public class Ords {
     private String riderPay;
     private String payRate;
     private String causeType;
+	public String getPolicyNo() {
+		return policyNo;
+	}
+	public void setPolicyNo(String policyNo) {
+		this.policyNo = policyNo;
+	}
+	public String getAccidentDate() {
+		return accidentDate;
+	}
+	public void setAccidentDate(String accidentDate) {
+		this.accidentDate = accidentDate;
+	}
+	public String getReceiveDate() {
+		return receiveDate;
+	}
+	public void setReceiveDate(String receiveDate) {
+		this.receiveDate = receiveDate;
+	}
+	public String getRiderType() {
+		return riderType;
+	}
+	public void setRiderType(String riderType) {
+		this.riderType = riderType;
+	}
+	public String getRiderSum() {
+		return riderSum;
+	}
+	public void setRiderSum(String riderSum) {
+		this.riderSum = riderSum;
+	}
+	public String getRiderPay() {
+		return riderPay;
+	}
+	public void setRiderPay(String riderPay) {
+		this.riderPay = riderPay;
+	}
+	public String getPayRate() {
+		return payRate;
+	}
+	public void setPayRate(String payRate) {
+		this.payRate = payRate;
+	}
+	public String getCauseType() {
+		return causeType;
+	}
+	public void setCauseType(String causeType) {
+		this.causeType = causeType;
+	}
+    
 }
