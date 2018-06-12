0d1ce901e927db241754cda224e1ca2c1efa1b6a
Samart Nuntakhwang <samart@ipassion.co.th>
2018-06-12 12:58:18 +0700

    add new jobs

---
+++ text2db/src/main/java/th/co/ipassion/testtool/dto/dm/Clrctrl.java
@@ -0,0 +1,153 @@
+package th.co.ipassion.testtool.dto.dm;
+
+public class Clrctrl {
+	private String rpno;
+	private String rpolicyno;
+	private String rcertno;
+	private String reffectivedate;
+	private String rpayperiod;
+	private String rpaydate;
+	private String rpremium;
+	private String rextraprem;
+	private String rsysdate;
+	private String rcurrentstatus;
+	private String roriginalstatus;
+	private String rmode;
+	private String time;
+	private String rrequestdate;
+	private String rsubmitno;
+	private String rgraceperiod;
+	private String rprinteddate;
+	private String rsubmitbranch;
+	private String ruserid;
+	private String rreasoncode;
+	private String rmoneyok;
+	public String getRpno() {
+		return rpno;
+	}
+	public void setRpno(String rpno) {
+		this.rpno = rpno;
+	}
+	public String getRpolicyno() {
+		return rpolicyno;
+	}
+	public void setRpolicyno(String rpolicyno) {
+		this.rpolicyno = rpolicyno;
+	}
+	public String getRcertno() {
+		return rcertno;
+	}
+	public void setRcertno(String rcertno) {
+		this.rcertno = rcertno;
+	}
+	public String getReffectivedate() {
+		return reffectivedate;
+	}
+	public void setReffectivedate(String reffectivedate) {
+		this.reffectivedate = reffectivedate;
+	}
+	public String getRpayperiod() {
+		return rpayperiod;
+	}
+	public void setRpayperiod(String rpayperiod) {
+		this.rpayperiod = rpayperiod;
+	}
+	public String getRpaydate() {
+		return rpaydate;
+	}
+	public void setRpaydate(String rpaydate) {
+		this.rpaydate = rpaydate;
+	}
+	public String getRpremium() {
+		return rpremium;
+	}
+	public void setRpremium(String rpremium) {
+		this.rpremium = rpremium;
+	}
+	public String getRextraprem() {
+		return rextraprem;
+	}
+	public void setRextraprem(String rextraprem) {
+		this.rextraprem = rextraprem;
+	}
+	public String getRsysdate() {
+		return rsysdate;
+	}
+	public void setRsysdate(String rsysdate) {
+		this.rsysdate = rsysdate;
+	}
+	public String getRcurrentstatus() {
+		return rcurrentstatus;
+	}
+	public void setRcurrentstatus(String rcurrentstatus) {
+		this.rcurrentstatus = rcurrentstatus;
+	}
+	public String getRoriginalstatus() {
+		return roriginalstatus;
+	}
+	public void setRoriginalstatus(String roriginalstatus) {
+		this.roriginalstatus = roriginalstatus;
+	}
+	public String getRmode() {
+		return rmode;
+	}
+	public void setRmode(String rmode) {
+		this.rmode = rmode;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getRrequestdate() {
+		return rrequestdate;
+	}
+	public void setRrequestdate(String rrequestdate) {
+		this.rrequestdate = rrequestdate;
+	}
+	public String getRsubmitno() {
+		return rsubmitno;
+	}
+	public void setRsubmitno(String rsubmitno) {
+		this.rsubmitno = rsubmitno;
+	}
+	public String getRgraceperiod() {
+		return rgraceperiod;
+	}
+	public void setRgraceperiod(String rgraceperiod) {
+		this.rgraceperiod = rgraceperiod;
+	}
+	public String getRprinteddate() {
+		return rprinteddate;
+	}
+	public void setRprinteddate(String rprinteddate) {
+		this.rprinteddate = rprinteddate;
+	}
+	public String getRsubmitbranch() {
+		return rsubmitbranch;
+	}
+	public void setRsubmitbranch(String rsubmitbranch) {
+		this.rsubmitbranch = rsubmitbranch;
+	}
+	public String getRuserid() {
+		return ruserid;
+	}
+	public void setRuserid(String ruserid) {
+		this.ruserid = ruserid;
+	}
+	public String getRreasoncode() {
+		return rreasoncode;
+	}
+	public void setRreasoncode(String rreasoncode) {
+		this.rreasoncode = rreasoncode;
+	}
+	public String getRmoneyok() {
+		return rmoneyok;
+	}
+	public void setRmoneyok(String rmoneyok) {
+		this.rmoneyok = rmoneyok;
+	}
+	
+	
+}
---
+++ text2db/src/main/java/th/co/ipassion/testtool/dto/dm/Clrprider.java
@@ -0,0 +1,34 @@
+package th.co.ipassion.testtool.dto.dm;
+
+public class Clrprider {
+	private String rpno;
+	private String ridertype;
+	private String riderpremium;
+	private String extrapremium;
+	public String getRpno() {
+		return rpno;
+	}
+	public void setRpno(String rpno) {
+		this.rpno = rpno;
+	}
+	public String getRidertype() {
+		return ridertype;
+	}
+	public void setRidertype(String ridertype) {
+		this.ridertype = ridertype;
+	}
+	public String getRiderpremium() {
+		return riderpremium;
+	}
+	public void setRiderpremium(String riderpremium) {
+		this.riderpremium = riderpremium;
+	}
+	public String getExtrapremium() {
+		return extrapremium;
+	}
+	public void setExtrapremium(String extrapremium) {
+		this.extrapremium = extrapremium;
+	}
+	
+	
+}
