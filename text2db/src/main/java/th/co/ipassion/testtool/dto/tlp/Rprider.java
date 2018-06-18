845513123e9e8a125c52bc8d2c643032f9d98733
Samart Nuntakhwang <samart@ipassion.co.th>
2018-06-18 19:32:47 +0700

    Add job rprider for tlp schema

---
+++ text2db/src/main/java/th/co/ipassion/testtool/dto/tlp/Rprider.java
@@ -0,0 +1,21 @@
+package th.co.ipassion.testtool.dto.tlp;
+
+public class Rprider {
+
+	private String receiptNo;
+	private String receiptDetail;
+	
+	public String getReceiptNo() {
+		return receiptNo;
+	}
+	public void setReceiptNo(String receiptNo) {
+		this.receiptNo = receiptNo;
+	}
+	public String getReceiptDetail() {
+		return receiptDetail;
+	}
+	public void setReceiptDetail(String receiptDetail) {
+		this.receiptDetail = receiptDetail;
+	}	
+	
+}
